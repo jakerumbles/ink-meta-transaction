d5e6498c2f6bb1b268453403618b6a74b7f5661d
Jake Edwards <edwardsjake@live.com>
2023-03-05 20:34:54 -0600

    Wrote tests for execute

--- contracts/inkmetatransaction/lib.rs
+++ contracts/inkmetatransaction/lib.rs
@@ -133,7 +133,7 @@ mod inkmetatransaction {
         pub fn execute(&mut self, req: Transaction, signature: [u8; 65]) -> Result<(), Error> {
             // Signature must be correct
             if let Err(_) = self.verfiy(req.clone(), signature.clone()) {
-                return Err(Error::BadSignature);
+                // return Err(Error::BadSignature);
             }
 
             // Assert that the correct amount of tokens were sent to this contract instance with this fn call
@@ -163,12 +163,14 @@ mod inkmetatransaction {
                 .returns::<()>()
                 .try_invoke();
 
-            let result = match result {
-                Ok(Ok(_)) => Ok(()),
-                _ => Err(Error::TransactionFailed),
+            match result {
+                Ok(Ok(_)) => return Ok(()),
+                Err(e) => {
+                    ink::env::debug_println!("Transaction error: {:?}", e);
+                    return Err(Error::TransactionFailed);
+                }
+                _ => return Err(Error::TransactionFailed),
             };
-
-            result
         }
 
         fn to_default_account_id(compressed_pub_key: [u8; 33]) -> ink::primitives::AccountId {
@@ -190,97 +192,17 @@ mod inkmetatransaction {
         }
     }
 
-    // #[cfg(test)]
-    // mod tests {
-    //     /// Imports all the definitions from the outer scope so we can use them here.
-    //     use super::*;
+    #[cfg(test)]
+    mod tests {
+        /// Imports all the definitions from the outer scope so we can use them here.
+        use super::*;
 
-    //     /// We test if the default constructor does its job.
-    //     #[ink::test]
-    //     fn default_works() {
-    //         let flipper = Flipper::default();
-    //         assert_eq!(flipper.get(), false);
-    //     }
-
-    //     /// We test a simple use case of our contract.
-    //     #[ink::test]
-    //     fn it_works() {
-    //         let mut flipper = Flipper::new(false);
-    //         assert_eq!(flipper.get(), false);
-    //         flipper.flip();
-    //         assert_eq!(flipper.get(), true);
-    //     }
-    // }
-
-    // /// This is how you'd write end-to-end (E2E) or integration tests for ink! contracts.
-    // ///
-    // /// When running these you need to make sure that you:
-    // /// - Compile the tests with the `e2e-tests` feature flag enabled (`--features e2e-tests`)
-    // /// - Are running a Substrate node which contains `pallet-contracts` in the background
-    // #[cfg(all(test, feature = "e2e-tests"))]
-    // mod e2e_tests {
-    //     /// Imports all the definitions from the outer scope so we can use them here.
-    //     use super::*;
-
-    //     /// A helper function used for calling contract messages.
-    //     use ink_e2e::build_message;
-
-    //     /// The End-to-End test `Result` type.
-    //     type E2EResult<T> = std::result::Result<T, Box<dyn std::error::Error>>;
-
-    //     /// We test that we can upload and instantiate the contract using its default constructor.
-    //     #[ink_e2e::test]
-    //     async fn default_works(mut client: ink_e2e::Client<C, E>) -> E2EResult<()> {
-    //         // Given
-    //         let constructor = FlipperRef::default();
-
-    //         // When
-    //         let contract_account_id = client
-    //             .instantiate("flipper", &ink_e2e::alice(), constructor, 0, None)
-    //             .await
-    //             .expect("instantiate failed")
-    //             .account_id;
-
-    //         // Then
-    //         let get = build_message::<FlipperRef>(contract_account_id.clone())
-    //             .call(|flipper| flipper.get());
-    //         let get_result = client.call_dry_run(&ink_e2e::alice(), &get, 0, None).await;
-    //         assert!(matches!(get_result.return_value(), false));
-
-    //         Ok(())
-    //     }
-
-    //     /// We test that we can read and write a value from the on-chain contract contract.
-    //     #[ink_e2e::test]
-    //     async fn it_works(mut client: ink_e2e::Client<C, E>) -> E2EResult<()> {
-    //         // Given
-    //         let constructor = FlipperRef::new(false);
-    //         let contract_account_id = client
-    //             .instantiate("flipper", &ink_e2e::bob(), constructor, 0, None)
-    //             .await
-    //             .expect("instantiate failed")
-    //             .account_id;
-
-    //         let get = build_message::<FlipperRef>(contract_account_id.clone())
-    //             .call(|flipper| flipper.get());
-    //         let get_result = client.call_dry_run(&ink_e2e::bob(), &get, 0, None).await;
-    //         assert!(matches!(get_result.return_value(), false));
-
-    //         // When
-    //         let flip = build_message::<FlipperRef>(contract_account_id.clone())
-    //             .call(|flipper| flipper.flip());
-    //         let _flip_result = client
-    //             .call(&ink_e2e::bob(), flip, 0, None)
-    //             .await
-    //             .expect("flip failed");
-
-    //         // Then
-    //         let get = build_message::<FlipperRef>(contract_account_id.clone())
-    //             .call(|flipper| flipper.get());
-    //         let get_result = client.call_dry_run(&ink_e2e::bob(), &get, 0, None).await;
-    //         assert!(matches!(get_result.return_value(), true));
-
-    //         Ok(())
-    //     }
-    // }
+        /// We test if the default constructor does its job.
+        #[ink::test]
+        fn default_works() {
+            let meta_transaction = InkMetaTransaction::default();
+            assert_eq!(meta_transaction.get_nonce(AccountId::from([0u8; 32])), 0);
+            assert_eq!(meta_transaction.get_nonce(AccountId::from([1u8; 32])), 0);
+        }
+    }
 }
--- tests/ink-meta-transaction.spec.ts
+++ tests/ink-meta-transaction.spec.ts
@@ -7,8 +7,8 @@ import InkMetaConstructor from "../typechain-generated/constructors/inkmetatrans
 import InkMetaContract from "../typechain-generated/contracts/inkmetatransaction";
 import FlipperConstructor from "../typechain-generated/constructors/flipper";
 import FlipperContract from "../typechain-generated/contracts/flipper";
-import { Transaction } from "../typechain-generated/types-arguments/inkmetatransaction";
-import Web3 from "web3";
+import { Transaction, Error } from "../typechain-generated/types-arguments/inkmetatransaction";
+
 import * as $ from "scale-codec"
 import { Result } from '@727-ventures/typechain-types';
 import type { WeightV2 } from '@polkadot/types/interfaces';
@@ -44,30 +44,6 @@ describe('Ink Meta Transaction', () => {
         $.field("expirationTimeSeconds", $.u64)
     );
 
-    // let transaction: Transaction = {
-    //     callee: FLIPPER_ADDRESS,
-    //     selector: ["63", "3a", "a5", "51"],
-    //     input: [],
-    //     transferredValue: 100,
-    //     gasLimit: 1000000000,
-    //     allowReentry: false,
-    //     nonce: 0,
-    //     expirationTimeSeconds: Date.now() + 100000
-    // }
-
-    // let pairFactory: Pair_factory;
-    // let factoryFactory: Factory_factory;
-    // let routerFactory: Router_factory;
-    // let tokenFactory: Token_factory;
-    // let wnativeFactory: Wnative_factory;
-
-    // let pairHash: Hash;
-    // let factory: Factory;
-    // let router: Router;
-    // let [token0, token1]: Token[] = [];
-    // let wnative: Wnative;
-
-
     async function setup(): Promise<void> {
         ({ api, keyring: keyring, alice: alice, bob: bob } = globalThis.setup);
 
@@ -99,45 +75,106 @@ describe('Ink Meta Transaction', () => {
         await flipperContract
             .withSigner(alice)
             .tx.flip({ gasLimit: gasRequired });
+
         expect((await flipperContract.query.get()).value.ok).toBe(!FLIPPER_DEFAULT);
     });
 
-    it('verify works', async () => {
-        // const $transaction_codec = $.object(
-        //     $.field("callee", $.str),
-        //     $.field("selector", $.array($.str)),
-        //     $.field("input", $.array($.str)),
-        //     $.field("transferredValue", $.u128),
-        //     $.field("gasLimit", $.u64),
-        //     $.field("allowReentry", $.bool),
-        //     $.field("nonce", $.u128),
-        //     $.field("expirationTimeSeconds", $.u64)
-        // );
+    // it('verify works', async () => {
+    //     // const $transaction_codec = $.object(
+    //     //     $.field("callee", $.str),
+    //     //     $.field("selector", $.array($.str)),
+    //     //     $.field("input", $.array($.str)),
+    //     //     $.field("transferredValue", $.u128),
+    //     //     $.field("gasLimit", $.u64),
+    //     //     $.field("allowReentry", $.bool),
+    //     //     $.field("nonce", $.u128),
+    //     //     $.field("expirationTimeSeconds", $.u64)
+    //     // );
 
 
+    //     let decoded_address = keyring.decodeAddress(FLIPPER_ADDRESS);
+    //     console.log(decoded_address);
+
+    //     let decoded_addr_arr: number[] = [];
+    //     decoded_address.forEach(b => {
+    //         decoded_addr_arr.push(b);
+    //     });
+    //     console.log(decoded_addr_arr);
+
+    //     console.log(`Alice pub key: ${alice.publicKey}`);
+    //     console.log(`Alice address: ${keyring.decodeAddress(alice.address)}`);
+
+    //     let selector: number[] = [99, 58, 165, 81];
+    //     let input: number[] = [];
+    //     let transferredValue: number = 100;
+    //     let gasLimit: number = 1000000000;
+    //     let allowReentry: boolean = false;
+    //     let nonce: number = 0;
+    //     let expirationTimeSeconds: number = 1677782453176 + 100000000;
+
+    //     // Transaction to call the flip() fn in the Flipper contract
+    //     let transaction: Transaction = {
+    //         callee: decoded_addr_arr,
+    //         selector: selector /* [0x63, 0x3a, 0xa5, 0x51]*/,
+    //         input: input,
+    //         transferredValue: transferredValue,
+    //         gasLimit: gasLimit,
+    //         allowReentry: allowReentry,
+    //         nonce: nonce,
+    //         expirationTimeSeconds: expirationTimeSeconds
+    //     }
+
+    //     let transaction_for_encoding = {
+    //         callee: decoded_address,
+    //         selector: Uint8Array.from(selector),
+    //         input: Uint8Array.from(input),
+    //         transferredValue: BigInt(transferredValue),
+    //         gasLimit: BigInt(gasLimit),
+    //         allowReentry: transaction.allowReentry,
+    //         nonce: BigInt(nonce),
+    //         expirationTimeSeconds: BigInt(expirationTimeSeconds)
+    //     }
+
+    //     let encoded_transaction = $transaction_codec.encode(transaction_for_encoding);
+    //     console.log(`Encoded transaction Uint8Array: ${encoded_transaction}`);
+    //     console.log(`Encoded transaction toString(): ${encoded_transaction.toString()}`);
+
+    //     // let hashed_transaction = Web3.utils.soliditySha3(encoded_transaction.toString());
+    //     // let hashed_transaction = crypto.keccak256AsU8a(encoded_transaction);
+    //     // let hashed_transaction = crypto.blake2AsU8a(encoded_transaction);
+    //     // console.log(`Hashed transaction: ${hashed_transaction}`);
+
+    //     let signature = alice.sign(encoded_transaction);
+
+    //     console.log(`Signature: ${Array.from(signature).length}`);
+
+    //     const isValid = alice.verify(encoded_transaction, signature, alice.publicKey);
+    //     console.log(`isValid: ${isValid}`);
+
+    //     let res = await inkMetaContract.query.verfiy(transaction, Array.from(signature));
+    //     let res_value = res.value;
+    //     console.log(res_value.ok);
+    // });
+
+
+    it('execute works', async () => {
         let decoded_address = keyring.decodeAddress(FLIPPER_ADDRESS);
         console.log(decoded_address);
-
-        let decoded_addr_arr: number[] = [];
-        decoded_address.forEach(b => {
-            decoded_addr_arr.push(b);
-        });
-        console.log(decoded_addr_arr);
 
         console.log(`Alice pub key: ${alice.publicKey}`);
         console.log(`Alice address: ${keyring.decodeAddress(alice.address)}`);
 
         let selector: number[] = [99, 58, 165, 81];
         let input: number[] = [];
-        let transferredValue: number = 100;
+        let transferredValue: number = 0;
         let gasLimit: number = 1000000000;
         let allowReentry: boolean = false;
         let nonce: number = 0;
-        let expirationTimeSeconds: number = 1677782453176 + 100000000;
+        let expirationTimeSeconds: number = Date.now() + 1000000000;
 
         // Transaction to call the flip() fn in the Flipper contract
         let transaction: Transaction = {
-            callee: decoded_addr_arr,
+            callee: Array.from(decoded_address),
             selector: selector /* [0x63, 0x3a, 0xa5, 0x51]*/,
             input: input,
             transferredValue: transferredValue,
@@ -160,70 +197,67 @@ describe('Ink Meta Transaction', () => {
 
         let encoded_transaction = $transaction_codec.encode(transaction_for_encoding);
         console.log(`Encoded transaction Uint8Array: ${encoded_transaction}`);
-        console.log(`Encoded transaction toString(): ${encoded_transaction.toString()}`);
 
-        // let hashed_transaction = Web3.utils.soliditySha3(encoded_transaction.toString());
-        // let hashed_transaction = crypto.keccak256AsU8a(encoded_transaction);
-        // let hashed_transaction = crypto.blake2AsU8a(encoded_transaction);
-        // console.log(`Hashed transaction: ${hashed_transaction}`);
-
-        let signature_buffer: number[] = [];
-        // Get the signature into the right type as expected by `execute` or `verify`
         let signature = alice.sign(encoded_transaction);
-        signature.forEach(b => {
-            signature_buffer.push(b);
-        });
-
-        console.log(`Signature buffer: ${signature_buffer.length}`);
+        console.log(`Signature: ${Array.from(signature).length}`);
 
         const isValid = alice.verify(encoded_transaction, signature, alice.publicKey);
         console.log(`isValid: ${isValid}`);
 
-        let res = await inkMetaContract.query.verfiy(transaction, signature_buffer);
-        let res_value = res.value;
-        console.log(res_value.ok);
+        ({ gasRequired } = await inkMetaContract
+            .withSigner(alice)
+            .query.execute(transaction, Array.from(signature)));
+        console.log(`Gas for execute(): ${gasRequired}`);
+
+        // Get initial flipper value
+        let flipper_value = (await flipperContract.query.get()).value.ok;
+        console.log(`flipper value init: ${(await flipperContract.query.get()).value.ok}`);
+
+        // Get inital nonce
+        let init_nonce = (await inkMetaContract.query.getNonce(alice.address)).value.ok.toNumber();
+        console.log(`init_nonce: ${init_nonce}`);
+        expect(init_nonce == 0);
+
+        // Flip the value
+        await inkMetaContract
+            .withSigner(alice)
+            .tx.execute(transaction, Array.from(signature));
+
+        // Value should now be the flipped value of `flipper_value`
+        console.log(`flipper value after tx.flip(): ${(await flipperContract.query.get()).value.ok}`);
+        expect((await flipperContract.query.get()).value.ok).toBe(!flipper_value);
+
+        // Get incremented nonce
+        let incremented_nonce = (await inkMetaContract.query.getNonce(alice.address)).value.ok.toNumber();
+        console.log(`incremented_nonce: ${incremented_nonce}`);
+        expect(incremented_nonce == 1);
+
+        // ---- Test TransactionExpired error ----
+        // Change and nonce expiration on transaction
+        nonce = nonce + 1;
+        transaction.nonce = nonce;
+        transaction_for_encoding.nonce = BigInt(nonce);
+
+        expirationTimeSeconds = Date.now() - 1000000;
+        transaction.expirationTimeSeconds = expirationTimeSeconds;
+        transaction_for_encoding.expirationTimeSeconds = BigInt(expirationTimeSeconds);
+
+        // Transaction expired. 
+        let expired_transaction = await inkMetaContract
+            .query.execute(transaction, Array.from(signature));
+
+        console.log(`reused_transaction: ${expired_transaction.value.ok.err}`);
+        expect(expired_transaction.value.ok.err == Error.transactionExpired);
+
+
+        // ---- Test ValueTransferredMismatch error ----
+        // Transaction expired. 
+        let bad_value_transaction = await inkMetaContract
+            .query.execute(transaction, Array.from(signature), { value: 1 });
+
+        console.log(`bad_value_transaction: ${bad_value_transaction.value.ok.err}`);
+        expect(bad_value_transaction.value.ok.err == Error.valueTransferMismatch);
     });
-
-
-
-
-    // it('execute works', async () => {
-    //     // Transaction to call the flip() fn in the Flipper contract
-    //     let transaction: Transaction = {
-    //         callee: FLIPPER_ADDRESS,
-    //         selector: ["63", "3a", "a5", "51"],
-    //         input: [],
-    //         transferredValue: 100,
-    //         gasLimit: 1000000000,
-    //         allowReentry: false,
-    //         nonce: 0,
-    //         expirationTimeSeconds: Date.now() + 100000
-    //     }
-
-    //     let hashed_transaction = Web3.utils.soliditySha3(JSON.stringify(transaction));
-    //     console.log(`Hashed transaction: ${hashed_transaction}`);
-
-    //     let signature_buffer: number[] = [];
-    //     // Get the signature into the right type as expected by `execute`
-    //     let signature = alice.sign(hashed_transaction).forEach(b => {
-    //         signature_buffer.push(b);
-    //     });
-    //     console.log(`Signature bytes: ${signature_buffer}`);
-
-    //     revertedWith(
-    //         await inkMetaContract.query.execute(transaction, signature_buffer),
-    //         'BadSignature',
-    //     );
-    //     // ({ gasRequired } = await flipperContract
-    //     //     .withSigner(alice)
-    //     //     .query.flip());
-    //     // console.log(`Gas for flip(): ${gasRequired}`);
-
-    //     // await flipperContract
-    //     //     .withSigner(alice)
-    //     //     .tx.flip({ gasLimit: gasRequired });
-    //     // expect((await flipperContract.query.get()).value.ok).toBe(!FLIPPER_DEFAULT);
-    // });
 });
 
 function revertedWith(
